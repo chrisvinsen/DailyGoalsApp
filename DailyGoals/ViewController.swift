801480cc117479bf3ef5d72226dd2fe71aec2841
Christianto Vinsen <christianto.vinsen@student.umn.ac.id>
2022-04-27 08:38:52 +0700

    Initial Commit

---
+++ DailyGoals/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DailyGoals
+//
+//  Created by Christianto Vinsen on 27/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
